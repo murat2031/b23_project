83cfc70c5f9015f56efcb90b6a7d851b4c357dd6
murat-2031 <murat_0301@icloud.com>
2021-07-19 16:44:08 -0400

    sdasd

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,11 @@ public class GitCool {
     // B23 is learning Git
     //B23 is a special number
     // B23 is making Some snese
+
+    // We have added our GitHub account under intelliJ
+    // make sure to click apply and click on to save
+
+    // we were able to successfully shared project under our github account
+    // Share Project On Github happen only one time per project
+    // it combines the 3 actions below
 }
